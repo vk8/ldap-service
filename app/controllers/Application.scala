366425ffdc03ad5b3f88e61a3289d8acf25afc81
vk8 <komkof@yandex.ru>
2015-04-02 17:13:53 +0400

    some exp with JSON

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,16 +1,17 @@
 package controllers
 
-import play.api.libs.json.JsArray
-import play.api.libs.json.JsNull
-import play.api.libs.json.JsNumber
-import play.api.libs.json.JsObject
-import play.api.libs.json.JsString
-import play.api.libs.json.JsValue
+import play.api.libs.functional.syntax.functionalCanBuildApplicative
+import play.api.libs.functional.syntax.toFunctionalBuilderOps
+import play.api.libs.functional.syntax.unlift
+import play.api.libs.json.JsError
+import play.api.libs.json.JsPath
 import play.api.libs.json.Json
-import play.api.mvc.Action
-import play.api.mvc.Controller
-import play.twirl.api.Html
+import play.api.libs.json.Json.toJsFieldJsValueWrapper
+import play.api.libs.json.Reads
 import play.api.libs.json.Writes
+import play.api.mvc.Action
+import play.api.mvc.BodyParsers
+import play.api.mvc.Controller
 
 object Application extends Controller {
 
@@ -20,72 +21,70 @@ object Application extends Controller {
 
   implicit val attributeWrites = new Writes[(String, Set[String])] {
     def writes(attribute: (String, Set[String])) = Json.obj(
-      //"name" -> attribute._1,
+      "name" -> attribute._1,
       "values" -> attribute._2)
   }
 
   def allSortedLdapAttributes = Action {
-    //val json = 
-    //Ok(Json.toJson(LdapUtils.usefulAttributesFinder)) List[(String, Set[String])]
     Ok(Json.prettyPrint(Json.toJson(LdapUtils.usefulAttributesFinder)))
   }
 
-  def main(args: Array[String]): Unit = {
-    val json: JsValue = Json.parse("""
-      {
-        "name" : "Watership Down",
-        "location" : {
-          "lat" : 51.235685,
-          "long" : -1.309197
-        },
-        "residents" : [ {
-          "name" : "Fiver",
-          "age" : 4,
-          "role" : null
-        }, {
-          "name" : "Bigwig",
-          "age" : 6,
-          "role" : "Owsla"
-        } ]
-      }
-      """)
-    val json2: JsValue = JsObject(Seq(
-      "name" -> JsString("Watership Down"),
-      "location" -> JsObject(Seq("lat" -> JsNumber(51.235685), "long" -> JsNumber(-1.309197))),
-      "residents" -> JsArray(Seq(
-        JsObject(Seq(
-          "name" -> JsString("Fiver"),
-          "age" -> JsNumber(4),
-          "role" -> JsNull)),
-        JsObject(Seq(
-          "name" -> JsString("Bigwig"),
-          "age" -> JsNumber(6),
-          "role" -> JsString("Owsla")))))))
-
-    val json3: JsValue = Json.obj(
-      "name" -> "Watership Down",
-      "location" -> Json.obj("lat" -> 51.235685, "long" -> -1.309197),
-      "residents" -> Json.arr(
-        Json.obj(
-          "name" -> "Fiver",
-          "age" -> 4,
-          "role" -> JsNull),
-        Json.obj(
-          "name" -> "Bigwig",
-          "age" -> 6,
-          "role" -> "Owsla")))
-
-    print(json)
-
-    // basic types
-    val jsonString = Json.toJson("Fiver")
-    val jsonNumber = Json.toJson(4)
-    val jsonBoolean = Json.toJson(false)
-
-    // collections of basic types
-    val jsonArrayOfInts = Json.toJson(Seq(1, 2, 3, 4))
-    val jsonArrayOfStrings = Json.toJson(List("Fiver", "Bigwig"))
+  def listPlaces = Action {
+    val json = Json.toJson(Place.list)
+    Ok(Json.prettyPrint(json))
   }
+
+  //  curl --include ^
+  //  --request POST ^
+  //  --header "Content-type: application/json" ^
+  //  --data "{\"name\":\"Nuthanger Farm\",\"location\":{\"lat\" : 51.244031,\"long\" : -1.263224}}" ^
+  //  http://localhost:9000/places
+  def savePlace = Action(BodyParsers.parse.json) { request =>
+    val placeResult = request.body.validate[Place]
+    placeResult.fold(
+      errors => {
+        BadRequest(Json.obj("status" -> "KO", "message" -> JsError.toFlatJson(errors)))
+      },
+      place => {
+        Place.save(place)
+        Ok(Json.obj("status" -> "OK", "message" -> ("Place '" + place.name + "' saved.")))
+      })
+  }
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+  case class Location(lat: Double, long: Double)
+
+  case class Place(name: String, location: Location)
+
+  object Place {
+
+    var list: List[Place] = List(
+      Place("Sandleford", Location(51.377797, -1.318965)),
+      Place("Watership Down", Location(51.235685, -1.309197)))
+
+    def save(place: Place) = {
+      list = list ::: List(place)
+    }
+  }
+
+  implicit val locationWrites: Writes[Location] = (
+    (JsPath \ "lat").write[Double] and
+    (JsPath \ "long").write[Double])(unlift(Location.unapply))
+
+  implicit val placeWrites: Writes[Place] = (
+    (JsPath \ "name").write[String] and
+    (JsPath \ "location").write[Location])(unlift(Place.unapply))
+
+  implicit val locationReads: Reads[Location] = (
+    (JsPath \ "lat").read[Double] and
+    (JsPath \ "long").read[Double])(Location.apply _)
+
+  implicit val placeReads: Reads[Place] = (
+    (JsPath \ "name").read[String] and
+    (JsPath \ "location").read[Location])(Place.apply _)
 }
 
 
