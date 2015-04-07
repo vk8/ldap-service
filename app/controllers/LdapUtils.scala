d040e26302fbf7d25927ba9a1045aea2a99b20b3
vk8 <komkof@yandex.ru>
2015-04-07 11:32:50 +0400

    rd some ldap attrs

--- app/controllers/LdapUtils.scala
+++ app/controllers/LdapUtils.scala
@@ -41,9 +41,9 @@ object LdapUtils {
     }
   }
 
-  private def getUsersData: Option[NamingEnumeration[SearchResult]] = getConnection() match {
+  private def getUsersData(attrs: Array[String]): Option[NamingEnumeration[SearchResult]] = getConnection() match {
     case Some(connection) => Some(connection.search(conf.getString("ldap.users-root"), conf.getString("ldap.filter"),
-      new SearchControls(SearchControls.SUBTREE_SCOPE, 0, 0, null, false, false)))
+      new SearchControls(SearchControls.SUBTREE_SCOPE, 0, 0, attrs, false, false)))
     case None => None
   }
 
@@ -51,7 +51,7 @@ object LdapUtils {
    * Usefulness is determined by the unique attribute values.
    * If the unique small (e.g. 1), it is likely that the attribute is not interesting.
    */
-  def usefulAttributesFinder: Map[String, Set[String]] = getUsersData match {
+  def usefulAttributesFinder: Map[String, Set[String]] = getUsersData(null) match {
     case Some(enumeration) =>
       var map = mutable.Map.empty[String, Set[String]]
       while (enumeration.hasMoreElements()) {
@@ -70,8 +70,25 @@ object LdapUtils {
     case None => Map.empty
   }
 
+  def xxx: Map[String, String] = getUsersData(Array("cn", "pwdLastSet")) match {
+    case Some(enumeration) =>
+      var map = Map.empty[String, String]
+      while (enumeration.hasMoreElements()) {
+        val user = enumeration.next().getAttributes().getAll
+        var list = List.empty[String]
+        while (user.hasMoreElements()) {
+          val attr = user.next()
+          val value = attr.get.toString
+          list = value :: list
+        }
+        if (list.size == 2) map += (list.head -> list.last)
+      }
+      map
+    case None => Map.empty
+  }
+
   def main(args: Array[String]): Unit = {
-    LdapUtils.usefulAttributesFinder
+    print(xxx)
   }
 
 }
